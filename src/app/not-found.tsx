216bcd2a4e57b8343414006e2b1bc9f79bae2988
ThomasBazin <tom.bazin@gmail.com>
2025-04-17 23:45:10 +0200

    feat: create custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-center gap-8 p-6">
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl text-white font-bold">Page not found...</h2>
+      </div>
+      <Button className="text-xl p-8">
+        <Link href={'/'}>Back to home</Link>
+      </Button>
+    </main>
+  );
+}
